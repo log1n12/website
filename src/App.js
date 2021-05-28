35e34c0b3bf414de2c702aea4218b5eee857f502
Carl Vincent <mr.carlvincent.bermundo@gmail.com>
2021-05-28 15:42:42 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
